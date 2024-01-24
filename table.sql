70733792d75948d2596552e37bc58053aa2a39d8
mluitens <m.luiting@gmail.com>
2024-01-24 10:07:34 +0100

    Table

---
+++ table.sql
@@ -0,0 +1,21 @@
+USE TestDB -
+
+
+CREATE TABLE Persons (
+	    PersonID int,
+	    LastName varchar(255),
+	    FirstName varchar(255),
+	    Address varchar(255),
+	    City varchar(255)
+);
+
+
+INSERT INTO Persons
+    (
+	        -- id column value is auto-generated
+        LastName,FirstName
+	    )
+	    VALUES
+	    (
+		       'John', 'Doe'        
+		    )    
